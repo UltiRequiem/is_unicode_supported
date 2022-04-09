488bf01d5b3aa610dc1a5802b6c0be479768c53c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-09 18:42:52 -0500

    :wrench: Update

--- scripts/node_support.ts
+++ scripts/node_support.ts
@@ -1,9 +1,10 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.12/node_support.ts";
 
 buildPackage({
-  repoName: "@ultirequiem/is-unicode-supported",
-  description: "Detect whether the terminal supports Unicode.",
-  homepage: "https://ulti.js.org/is_unicode_supported",
-  keywords: ["unicode", "terminal"],
-  version: "2.0.0",
+	repoName: "is_unicode_supported",
+	name: "@ultirequiem/is-unicode-supported",
+	description: "Detect whether the terminal supports Unicode.",
+	homepage: "https://ulti.js.org/is_unicode_supported",
+	keywords: ["unicode", "terminal"],
+	version: "2.0.1",
 });
