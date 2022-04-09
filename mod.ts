ea89a6b5425f101ab638f63e9f60a9891bf5a1d3
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-09 09:09:06 -0500

    :art: Format

--- mod.ts
+++ mod.ts
@@ -7,16 +7,16 @@ isUnicodeSupported(); //=> true
 ```
 */
 export function isUnicodeSupported() {
-	if (Deno.build.os !== "windows") {
-		return Deno.env.get("TERM") !== "linux";
-	}
+  if (Deno.build.os !== "windows") {
+    return Deno.env.get("TERM") !== "linux";
+  }
 
-	return (
-		Boolean(Deno.env.get("CI")) ||
-		Boolean(Deno.env.get("WT_SESSION")) || // Windows Terminal
-		Deno.env.get("ConEmuTask") === "{cmd::Cmder}" || // ConEmu and cmder
-		Deno.env.get("TERM_PROGRAM") === "vscode" ||
-		Deno.env.get("TERM") === "xterm-256color" ||
-		Deno.env.get("TERM") === "alacritty"
-	);
+  return (
+    Boolean(Deno.env.get("CI")) ||
+    Boolean(Deno.env.get("WT_SESSION")) || // Windows Terminal
+    Deno.env.get("ConEmuTask") === "{cmd::Cmder}" || // ConEmu and cmder
+    Deno.env.get("TERM_PROGRAM") === "vscode" ||
+    Deno.env.get("TERM") === "xterm-256color" ||
+    Deno.env.get("TERM") === "alacritty"
+  );
 }
--- scripts/node_support.ts
+++ scripts/node_support.ts
@@ -1,9 +1,9 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.12/node_support.ts";
 
 buildPackage({
-	repoName: "@ultirequiem/is-unicode-supported",
-	description: "Detect whether the terminal supports Unicode.",
-	homepage: "https://ulti.js.org/is_unicode_supported",
-	keywords: ["unicode", "terminal"],
-	version: "2.0.0",
+  repoName: "@ultirequiem/is-unicode-supported",
+  description: "Detect whether the terminal supports Unicode.",
+  homepage: "https://ulti.js.org/is_unicode_supported",
+  keywords: ["unicode", "terminal"],
+  version: "2.0.0",
 });
